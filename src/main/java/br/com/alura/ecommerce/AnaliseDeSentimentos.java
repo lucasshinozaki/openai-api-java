9080dff561e0a9d592a540749185d3334f370b63
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-06-10 16:02:00 -0300

    Chamada em lote

--- src/main/java/br/com/alura/ecommerce/AnaliseDeSentimentos.java
+++ src/main/java/br/com/alura/ecommerce/AnaliseDeSentimentos.java
@@ -10,10 +10,13 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class AnaliseDeSentimentos {
     public static void main(String[] args) {
-        var promptSistema = """
+        try {
+
+            var promptSistema = """
                 Você é um analisador de sentimentos de avaliação de produtos.
                 Escreva um parágrafo com até 50 palavras resumindo as avaliações e depois atribua qual o sentimento geral para o produto
                 Identifique também 3 pontos fortes e 3 pontos fracos idenficados a partir das avaliações
@@ -26,35 +29,43 @@ public class AnaliseDeSentimentos {
                 Pontos fracos: [3 bullets points]
                 """;
 
-        var produto = "tapete-de-yoga";
+            var diretorioAvaliacoes = Path.of("src/main/resources/avaliacoes");
+            var arquivosDeAvaliacoes = Files
+                    .walk(diretorioAvaliacoes, 1)
+                    .filter(path -> path.toString().endsWith(".txt"))
+                    .collect(Collectors.toList());
 
-        var promptUsuario = carregarArquivo(produto);
+            for (Path arquivo: arquivosDeAvaliacoes) {
+                var promptUsuario = carregarArquivo(arquivo);
 
-        var request = ChatCompletionRequest
-                .builder()
-                .model("gpt-3.5-turbo")
-                .messages(Arrays.asList(
-                        new ChatMessage(
-                                ChatMessageRole.SYSTEM.value(),
-                                promptSistema),
-                        new ChatMessage(
-                                ChatMessageRole.USER.value(),
-                                promptUsuario)))
-                .build();
+                var request = ChatCompletionRequest
+                        .builder()
+                        .model("gpt-3.5-turbo")
+                        .messages(Arrays.asList(
+                                new ChatMessage(
+                                        ChatMessageRole.SYSTEM.value(),
+                                        promptSistema),
+                                new ChatMessage(
+                                        ChatMessageRole.USER.value(),
+                                        promptUsuario)))
+                        .build();
 
-        var chave = System.getenv("OPENAI_API_KEY");
-        var service = new OpenAiService(chave, Duration.ofSeconds(60));
+                var chave = System.getenv("OPENAI_API_KEY");
+                var service = new OpenAiService(chave, Duration.ofSeconds(60));
 
-        var resposta = service
-                .createChatCompletion(request)
-                .getChoices().get(0).getMessage().getContent();
+                var resposta = service
+                        .createChatCompletion(request)
+                        .getChoices().get(0).getMessage().getContent();
 
-        salvarAnalise(produto, resposta);
+                salvarAnalise(arquivo.getFileName().toString().replace(".txt", ""), resposta);
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro ao fazer analise de sentimento " + e);
+        }
     }
-    private static String carregarArquivo(String arquivo) {
+    private static String carregarArquivo(Path arquivo) {
         try {
-            var path = Path.of("src/main/resources/avaliacoes/avaliacoes-" + arquivo);
-            return Files.readAllLines(path).toString();
+            return Files.readAllLines(arquivo).toString();
         } catch (Exception e) {
             throw new RuntimeException("Erro ao carregar arquivo ", e);
         }
